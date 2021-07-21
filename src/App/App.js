e0bb45c3cd72b85c31be96f6adfde278dd3f1dc8
Shayan <70605985+shayan-golafshani@users.noreply.github.com>
2021-07-21 10:44:27 -0600

    pass res down

--- src/App/App.js
+++ src/App/App.js
@@ -31,7 +31,7 @@ class App extends Component {
       <div className="App">
         <h1 className='app-title'>Turing Cafe Reservations</h1>
         <div className='resy-form'>
-          <Form />
+          <Form addReservation={this.addReservation}/>
         </div>
         <div className='resy-container'>
           <Reservations reservations={this.state.reservations} />
